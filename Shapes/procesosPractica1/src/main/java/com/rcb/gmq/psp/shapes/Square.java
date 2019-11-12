3bd441a531449fd2d28a235f673cb5227e11ee2e
Raul Camarero <raul.camarero@fon.com>
2019-11-12 09:26:51 +0100

    Shapes exercice

---
+++ Shapes/procesosPractica1/src/main/java/com/rcb/gmq/psp/shapes/Square.java
@@ -0,0 +1,18 @@
+package com.rcb.gmq.psp.shapes;
+
+public class Square extends FourSides implements Shapes {
+
+    public Square(int side) {
+        super(side, side);
+    }
+
+    @Override
+    public double calculateArea() {
+        return super.calculateArea();
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return super.calculatePerimeter();
+    }
+}
