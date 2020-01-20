80af706d7808c71187d724c6dc9d65daa4e335b1
Raul Camarero <raul.camarero@fon.com>
2020-01-20 08:59:55 +0100

    Server upd

---
+++ T3-ProgramacionDeComunicacionesEnRed/Ejercicios/src/com/rcb/gmq/psp/sockets/udp/ServerUdpApplication.java
@@ -0,0 +1,35 @@
+package com.rcb.gmq.psp.sockets.udp;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.*;
+import java.time.Instant;
+
+import static com.rcb.gmq.psp.sockets.udp.UdpConnectionProperties.PORT;
+
+public class ServerUdpApplication {
+
+    public static void main(String... args) throws SocketException {
+
+        DatagramSocket server = null;
+        try {
+
+            server = new DatagramSocket(PORT);
+            InetAddress address = InetAddress.getByName("localhost");
+
+            while(true) {
+                String buffer = "La fecha actual es : "+ Instant.now().toString();
+                DatagramPacket data = new DatagramPacket(buffer.getBytes(),buffer.length(), address, PORT);
+                server.send(data);
+                Thread.sleep(1000);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            server.close();
+        }
+    }
+}
---
+++ T3-ProgramacionDeComunicacionesEnRed/Ejercicios/src/com/rcb/gmq/psp/sockets/udp/UdpConnectionProperties.java
@@ -0,0 +1,6 @@
+package com.rcb.gmq.psp.sockets.udp;
+
+public class UdpConnectionProperties {
+
+    public final static int PORT = 8080;
+}
